f8fba6e074e34fc136dc0ca1de77593c08e1c7fa
R1nge <r1ngegamedev@gmail.com>
2024-11-30 14:26:01 +0300

    Fps

--- main.cpp
+++ main.cpp
@@ -2,26 +2,27 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(720, 1280), "Flappy Bird");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+    float fps = 60;
+    float frameTime = static_cast<float>(1) / fps;
+    sf::Clock frame_clock;
     while (window.isOpen())
     {
+        if (frame_clock.getElapsedTime().asMilliseconds() >= frameTime) {
+            frame_clock.restart();
+            shape.move(0, 1);
+        }
+
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            {
-                shape.move(-1.f, 0.f);
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Right))
-            {
-                shape.move(1.f, 0.f);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+                shape.move(0, -1);
             }
         }
 
