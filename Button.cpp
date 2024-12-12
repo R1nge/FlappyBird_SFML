fce4aefbb3a8845f3b2f6722074effccff6088fa
R1nge <r1ngegamedev@gmail.com>
2024-12-12 21:00:30 +0300

    feat(button UI): center rect

--- Button.cpp
+++ Button.cpp
@@ -4,7 +4,7 @@
 Button::Button(sf::Vector2f center, sf::Vector2f size)
 {
 	_rect.setFillColor(sf::Color::Black);
-	_rect.setPosition(center);
+	_rect.setPosition(center - sf::Vector2f(size.x / 2, size.y / 2));
 	_rect.setSize(size);
 }
 
